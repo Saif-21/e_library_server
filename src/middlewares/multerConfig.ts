305fa8f74dde85b21452eaf1e22f5f480f13833b
Saif-21 <saif348900@gmail.com>
2024-09-18 00:18:10 +0530

    multer configure (#15)

---
+++ src/middlewares/multerConfig.ts
@@ -0,0 +1,19 @@
+import multer from "multer";
+import path from "node:path";
+
+const bookStorage = multer.diskStorage({
+    destination: path.resolve(__dirname, "../../public/data/uploads"),
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "_" + file.originalname.split(" ").join("_"));
+    },
+});
+
+const bookUpload = multer({
+    storage: bookStorage,
+    limits: { fileSize: 30e7 }, // 30 mb 30 * 1024 * 1024 ...
+}).fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "file", maxCount: 1 },
+]);
+
+export { bookUpload };
