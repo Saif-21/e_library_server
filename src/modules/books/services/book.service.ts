690a34dabb371f76ac7af63a216ac85638303868
Saif-21 <saif348900@gmail.com>
2024-09-21 20:02:01 +0530

    coverImage upload service implemented

--- src/modules/books/services/book.service.ts
+++ src/modules/books/services/book.service.ts
@@ -1,8 +1,13 @@
 import path from "node:path";
 import cloudinary from "../../../config/cloudinary";
-import { BookData } from "../types/book.types";
+import { BookData, RequestFileData } from "../types/book.types";
+import ApiError from "../../../utils/apiError";
 
 class BookService {
+    private BOOK_COVER_IMAGE = "cover";
+    private BOOK_PDF = "pdf";
+    private BOOK_COVER_IMAGE_FOLDER_NAME = "cover-images";
+    private BOOK_PDF_FOLDER_NAME = "book-pdfs";
     // createBook(data: object) {
     //     return {
     //         success: true,
@@ -13,23 +18,65 @@ class BookService {
     // }
 
     async createBookRecord(files: BookData) {
-        const fileName = files.coverImage[0].filename;
-        const coverImage = files.coverImage[0];
-        const coverImageMimeType = files.coverImage[0].mimetype
-            .split("/")
-            .at(-1);
-
-        const filePath = path.resolve(
-            __dirname,
-            "../../../../public/data/uploads",
-            fileName
+        const coverImageUploadData = await this.uploadFilesToCloudService(
+            files.coverImage[0],
+            this.BOOK_COVER_IMAGE
         );
-        const uploadResult = await cloudinary.uploader.upload(filePath, {
-            filename_override: fileName,
-            folder: "book-covers",
-            format: coverImageMimeType,
-        });
-        console.log(uploadResult);
+
+        // const bookPdfUpladData = await this.uploadFilesToCloudService(
+        //     files.bookPdf[0],
+        //     this.BOOK_PDF
+        // );
+
+        // console.log(bookPdfUpladData);
+        console.log(coverImageUploadData);
+    }
+
+    private async uploadFilesToCloudService(
+        file: RequestFileData,
+        filetype: string
+    ) {
+        try {
+            const fileName = file.filename;
+            const coverImageMimeType = file.mimetype.split("/").at(-1);
+            const filePath = path.resolve(
+                __dirname,
+                "../../../../public/data/uploads",
+                fileName
+            );
+            let folderName = "";
+            if (filetype === this.BOOK_COVER_IMAGE) {
+                folderName = this.BOOK_COVER_IMAGE_FOLDER_NAME;
+            }
+
+            const uploadResult = await cloudinary.uploader.upload(filePath, {
+                filename_override: fileName,
+                folder: folderName,
+                format: coverImageMimeType,
+            });
+
+            if (uploadResult) {
+                return uploadResult.secure_url;
+            }
+
+            return false;
+        } catch (error) {
+            console.log(error);
+            let errorMessage = "Something went wrong while uploading files.";
+            if (error instanceof Error) {
+                errorMessage = error.message || errorMessage;
+            } else if (typeof error === "string") {
+                errorMessage = error;
+            } else if (
+                error &&
+                typeof error === "object" &&
+                "message" in error
+            ) {
+                errorMessage = (error as { message: string }).message;
+            }
+
+            throw ApiError.internalServer(errorMessage);
+        }
     }
 }
 
